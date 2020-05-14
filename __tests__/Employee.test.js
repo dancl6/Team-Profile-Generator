c313d2413a85a4002955ad2bfdc8316a798010bd
Dan Lyons <danielclyons@yahoo.com>
2020-05-14 14:26:44 -0700

    add employee tests, failed tests, passed tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -12,5 +12,6 @@ test("get employee's info methods", () => {
   const employee = new Employee('Dan Lyons', 'abc','dan@yahoo.com');
   expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));   
   expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+  expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
   expect(employee.getRole()).toEqual("Employee");
 })
