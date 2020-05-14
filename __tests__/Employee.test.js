2bfae677d2f95e606b38752cdb83e65616212795
Dan Lyons <danielclyons@yahoo.com>
2020-05-14 14:22:35 -0700

    added employee test, failed test, passed test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,16 @@
+const Employee = require('../lib/Employee.js')
+
+test("gets employee's info properties", () => {
+  const employee = new Employee('Dan Lyons', 'abc','dan@yahoo.com');
+  // const engineer = new Engineer('dan@yahoo.com');
+  expect(employee.name).toEqual(expect.any(String));
+  expect(employee.id).toEqual(expect.any(String));
+  expect(employee.email).toEqual(expect.any(String));
+});
+
+test("get employee's info methods", () => {
+  const employee = new Employee('Dan Lyons', 'abc','dan@yahoo.com');
+  expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));   
+  expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+  expect(employee.getRole()).toEqual("Employee");
+})
