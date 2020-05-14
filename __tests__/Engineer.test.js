392606f918cca6f11b220d8331b1bce28dee2be5
Dan Lyons <danielclyons@yahoo.com>
2020-05-14 11:50:44 -0700

    updated Engineer test, failed, then passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,6 +1,12 @@
 const Engineer = require('../lib/Engineer.js')
 
-test("gets engineer's email", () => {
-  const engineer = new Engineer('dan@yahoo.com');
+test("gets engineer's info", () => {
+  const engineer = new Engineer('Dan Lyons', 'abc','dan@yahoo.com', 123456789);
+  // const engineer = new Engineer('dan@yahoo.com');
+  expect(engineer.name).toEqual(expect.any(String));
+  expect(engineer.id).toEqual(expect.any(String));
   expect(engineer.email).toEqual(expect.any(String));
+  expect(engineer.number).toEqual(expect.any(Number));
+
+
 });
